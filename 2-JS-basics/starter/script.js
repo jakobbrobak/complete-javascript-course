e7622ca7e009532edc818ccf0d590346c2b088c5
Jakob Brobak <jakobbrobak@gmail.com>
2019-02-18 12:47:42 +0100

    Initial commit

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,10 @@
+var firstName = 'John';
+var lastName = 'Smith';
+var age = 28;
+var fullAge = true;
+
+var job;
+
+job = 'Web Developer';
+console.log(fullAge + job);
+console.log(firstName + ' ' + lastName + ' is ' + age + ' years old');
